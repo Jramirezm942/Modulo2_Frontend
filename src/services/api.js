3b7f1b7f970344ce43339c91d4af7e2a58675169
Gala Torres <galatorresrovelo@gmail.com>
2021-01-13 18:50:02 -0800

    Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+//valida si la aplicacion esta en produccion
+const isProduction = process.env.NODE_ENV === "production";
 
-const URL = "http://localhost:3000/api";
+// si la app esta ya en produccion colocara otra ruta de lo contrario usara localhost
+const URL =  isProduction ? "https://managertracker.herokuapp.com/api" : "http://localhost:3000/api";
+
 
 axios.defaults.withCredentials = true;
 
